f71599fa7c88556ac05d41382227cee50b007bad
Ivo Maixner <ivo.maixner@gmail.com>
2020-03-24 09:41:25 +0100

    chore: Generate Spring Boot backend from Initializr

---
+++ pivospolu-backend/src/main/java/cz/lastware/pivospolu/PivospoluBackendApplication.java
@@ -0,0 +1,13 @@
+package cz.lastware.pivospolu;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PivospoluBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PivospoluBackendApplication.class, args);
+	}
+
+}
---
+++ pivospolu-backend/src/test/java/cz/lastware/pivospolu/PivospoluBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package cz.lastware.pivospolu;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PivospoluBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
