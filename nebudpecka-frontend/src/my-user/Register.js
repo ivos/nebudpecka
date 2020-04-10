d6eae27519da3ecb94fed4dab41d2f1aff1254b8
Ivo Maixner <ivo.maixner@gmail.com>
2020-04-10 13:44:32 +0200

    feat: Beautify Register screen

--- nebudpecka-frontend/src/my-user/Register.js
+++ nebudpecka-frontend/src/my-user/Register.js
@@ -1,5 +1,5 @@
 import React from 'react'
-import { Button, Col, Form, Row } from 'react-bootstrap'
+import { Button, Card, Col, Form, Row } from 'react-bootstrap'
 import * as Yup from 'yup'
 import { register } from '../api'
 import { FieldGroup, FormikForm } from '../form'
@@ -13,38 +13,42 @@ const mapServerErrorCodeToLabel = (field, code) => {
 
 export default () => {
   return (
-    <div>
-      Register
+    <Card>
+      <Card.Body>
+        <Card.Title>
+          Registrace
+        </Card.Title>
 
-      <FormikForm
-        initialValues={{ email: '', password: '', name: '' }}
-        validationSchema={
-          Yup.object({
-            email: Yup.string()
-              .email()
-              .required(),
-            password: Yup.string()
-              .min(8)
-              .required(),
-            name: Yup.string()
-              .required()
-          })
-        }
-        onSubmit={register}
-        mapServerErrorCodeToLabel={mapServerErrorCodeToLabel}
-      >
-        <FieldGroup as={Form.Control} name="email" label="Email" sm={[2, 9]}
-                    isValid={false} autoFocus/>
-        {/*type="password"*/}
-        <FieldGroup as={Form.Control} name="password" label="Password" sm={[2, 9]}/>
-        <FieldGroup as={Form.Control} name="name" label="Name" sm={[2, 9]}/>
+        <FormikForm
+          initialValues={{ email: '', password: '', name: '' }}
+          validationSchema={
+            Yup.object({
+              email: Yup.string()
+                .email()
+                .required(),
+              password: Yup.string()
+                .min(8)
+                .required(),
+              name: Yup.string()
+                .required()
+            })
+          }
+          onSubmit={register}
+          mapServerErrorCodeToLabel={mapServerErrorCodeToLabel}
+        >
+          <FieldGroup as={Form.Control} name="email" label="Email" sm={[2, 9]}
+                      isValid={false} autoFocus autoComplete="username"/>
+          <FieldGroup as={Form.Control} name="password" label="Heslo" sm={[2, 9]}
+                      type="password" autoComplete="current-password"/>
+          <FieldGroup as={Form.Control} name="name" label="Jméno" sm={[2, 9]}/>
 
-        <Form.Group as={Row}>
-          <Col sm={{ offset: 2 }}>
-            <Button type="submit">Zaregistrovat se</Button>
-          </Col>
-        </Form.Group>
-      </FormikForm>
-    </div>
+          <Form.Group as={Row}>
+            <Col sm={{ offset: 2 }}>
+              <Button type="submit">Zaregistrovat se</Button>
+            </Col>
+          </Form.Group>
+        </FormikForm>
+      </Card.Body>
+    </Card>
   )
 }
