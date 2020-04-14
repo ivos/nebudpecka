36ba41bb5de486138b952655b5697d0a891cd1a4
Ivo Maixner <ivo.maixner@gmail.com>
2020-04-14 14:54:49 +0200

    chore: Add Font Awesome

--- nebudpecka-frontend/src/index.js
+++ nebudpecka-frontend/src/index.js
@@ -5,6 +5,12 @@ import './index.css'
 import App from './App'
 import * as serviceWorker from './serviceWorker'
 
+import { library } from '@fortawesome/fontawesome-svg-core'
+import { faSpinner } from '@fortawesome/free-solid-svg-icons'
+// import { x as farX } from '@fortawesome/free-regular-svg-icons'
+
+library.add(faSpinner)
+
 require('./i18n')
 
 ReactDOM.render(
